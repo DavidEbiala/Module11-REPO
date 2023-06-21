3d891885c0effcf7b323a1986bf00c71d80b1ded
Zino Ebiala <eebia002@fiu.edu>
2023-06-21 19:26:42 -0400

    fixed and added middleware

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
+const dbData = require('./db/db.json');
 
 const app = express();
 const PORT = 3001;
 
 // Focus on this app.use.
-app.use();
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/'));
+  });
 //Focus on this too.
-app.get('/notes',(req, res) => );
+app.get('/notes',(req, res) => res.json(dbData) );
 
 //And this last one too.
 app.listen(PORT, () =>
