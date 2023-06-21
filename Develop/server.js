c0290f269339652e63cdfe38e08c63e01b38e0cd
Zino Ebiala <eebia002@fiu.edu>
2023-06-21 17:54:13 -0400

    Added starter code

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+// Focus on this app.use.
+app.use();
+
+//Focus on this too.
+app.get();
+
+//And this last one too.
+app.listen();
