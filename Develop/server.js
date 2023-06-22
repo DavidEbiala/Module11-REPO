fe9de7b07acd70cb1a68e02d8f489c67ceac423f
Zino Ebiala <eebia002@fiu.edu>
2023-06-21 22:21:32 -0400

    Fixed website funcitoning, need to make notes save to db

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const path = require('path');
 const dbData = require('./db/db.json');
-
+const PORT = process.env.port || 3001;
 const app = express();
-const PORT = 3001;
+
 
 // Focus on this app.use.
 app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/'));
   });
 //Focus on this too.
-app.get('/notes',(req, res) => res.json(dbData) );
-
+app.get('/notes',(req, res) => {
+    console.info(`GET /api/notes`)
+    res.sendFile(path.join(__dirname, '/public/notes.html') );
+});
+app.post('/notes', (req, res) => {
+    console.info(`${req.method} request recieved to add a note.`);
+    res.sendFile(path.join(__dirname, '/public/notes.html') );
+})
 //And this last one too.
 app.listen(PORT, () =>
     console.info(`Example app listening at http://localhost:${PORT} 🚀`)
